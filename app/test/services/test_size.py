b6495bf9d2138822be7b0f35be968e7376379dd4
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-08-31 13:24:25 -0500

    adding test for the create_size service

--- app/test/services/test_size.py
+++ app/test/services/test_size.py
@@ -7,3 +7,12 @@ def test_get_sizes_service(client, create_sizes, size_uri):
     returned_sizes = {size["_id"]: size for size in response.json}
     for size in create_sizes:
         pytest.assume(size["_id"] in returned_sizes)
+
+
+def test_create_size_service(client, size_uri, size):
+    response = client.post(size_uri, json=size)
+    pytest.assume(response.status.startswith("200"))
+    returned_size = response.json
+    pytest.assume(returned_size["_id"])
+    pytest.assume(returned_size["name"] == size["name"])
+    pytest.assume(returned_size["price"] == size["price"])
