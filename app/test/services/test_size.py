474102fc3ae1a2ef8741b050d194c33848ed2691
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-08-31 15:21:21 -0500

    feat: adding id for the update size service

--- app/test/services/test_size.py
+++ app/test/services/test_size.py
@@ -26,3 +26,12 @@ def test_update_size_service(client, create_size, size_uri, size):
     pytest.assume(returned_size["_id"] == create_size.json["_id"])
     pytest.assume(returned_size["name"] == size["name"])
     pytest.assume(returned_size["price"] == size["price"])
+
+def test_get_size_by_id_service(client, create_size, size_uri):
+    size_id = create_size.json["_id"]
+    response = client.get(f'{size_uri}id/{size_id}')
+    pytest.assume(response.status.startswith("200"))
+    returned_size = response.json
+    pytest.assume(returned_size["_id"] == create_size.json["_id"])
+    pytest.assume(returned_size["name"] == create_size.json["name"])
+    pytest.assume(returned_size["price"] == create_size.json["price"])
