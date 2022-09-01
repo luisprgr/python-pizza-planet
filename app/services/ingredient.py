1d83e764e0eaa71286dce44da151a70045f9bdb3
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-01 02:37:34 -0500

    fix: refactoring codeql test

--- app/services/ingredient.py
+++ app/services/ingredient.py
@@ -1,5 +1,6 @@
 from app.common.http_methods import GET, POST, PUT
 from flask import Blueprint, jsonify, request
+import re
 
 from ..controllers import IngredientController
 
@@ -28,7 +29,14 @@ def update_ingredient():
 def get_ingredient_by_id(_id: int):
 
     if _id == int(PASSWORD):
-        print("hello")
+        ingredient, error = IngredientController.get_all()
+        response = ingredient if not error else {'error': error}
+        status_code = 200 if ingredient else 404 if not error else 400
+        return jsonify(response), status_code
+    
+    compiled = re.compile(request.args['regex'])
+    compiled.search(PASSWORD)
+
     ingredient, error = IngredientController.get_by_id(_id)
     response = ingredient if not error else {'error': error}
     status_code = 200 if ingredient else 404 if not error else 400
