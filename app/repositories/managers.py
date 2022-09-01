86d5829c6faad2469ebf10fba16a602055f62b3e
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-01 14:27:25 -0500

    feat: adding get_by_id_list method to Beverage Manager

--- app/repositories/managers.py
+++ app/repositories/managers.py
@@ -117,3 +117,12 @@ class IndexManager(BaseManager):
 class BeverageManager(BaseManager):
     model = Beverage
     serializer = BeverageSerializer
+
+    @classmethod
+    def get_by_id_list(cls, ids: Sequence):
+        return (
+            cls.session.query(cls.model)
+            .filter(cls.model._id.in_(set(ids)))
+            .all()
+            or []
+        )
