1b03fcb55095150ee343d12b1999d7d4fca388ef
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-01 10:39:04 -0500

    fix: linter error in test_beverage

--- app/test/services/test_beverage.py
+++ app/test/services/test_beverage.py
@@ -1,9 +1,12 @@
 import pytest
 
+
 def test_get_beverages_service(client, create_beverages, beverage_uri):
     response = client.get(beverage_uri)
     pytest.assume(response.status.startswith("200"))
-    returned_beverages = {beverage["_id"]: beverage for beverage in response.json}
+    returned_beverages = {
+        beverage["_id"]: beverage for beverage in response.json
+    }
     for beverage in create_beverages:
         pytest.assume(beverage["_id"] in returned_beverages)
 
@@ -14,10 +17,12 @@ def test_create_beverage_service(client, beverage, beverage_uri):
     returned_beverage = response.json
     pytest.assume(returned_beverage["_id"])
     pytest.assume(returned_beverage["name"] == beverage["name"])
-    pytest.assume(returned_beverage["price"] == beverage["price"]) 
+    pytest.assume(returned_beverage["price"] == beverage["price"])
 
 
-def test_update_beverage_service(client, create_beverage, beverage, beverage_uri):
+def test_update_beverage_service(
+    client, create_beverage, beverage, beverage_uri
+):
     beverage_id = create_beverage.json["_id"]
     response = client.put(f'{beverage_uri}{beverage_id}', json=beverage)
     pytest.assume(response.status.startswith("200"))
@@ -25,6 +30,7 @@ def test_update_beverage_service(client, create_beverage, beverage, beverage_uri
     pytest.assume(returned_beverage["_id"] == create_beverage.json["_id"])
     pytest.assume(returned_beverage["name"] == beverage["name"])
     pytest.assume(returned_beverage["price"] == beverage["price"])
+
 
 def test_get_beverage_by_id_service(client, create_beverage, beverage_uri):
     beverage_id = create_beverage.json["_id"]
