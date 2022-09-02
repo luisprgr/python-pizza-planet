f3c2ac8e73d02da2d870d27da97f2d78722ba19e
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-01 21:43:14 -0500

    feat: seeding order table

--- seeder.py
+++ seeder.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+import random
 from app.repositories.managers import (
     SizeManager,
     BeverageManager,
@@ -20,249 +22,309 @@ INGREDIENT_MIN_VALUE = 1.00
 BEVERAGE_MAX_VALUE = 5.00
 BEVERAGE_MIN_VALUE = 0.50
 
+MIN_DATE = datetime.date(2022, 1, 1)
+MAX_DATE = datetime.date(2022, 12, 31)
+
+NUMBER_OF_CLIENTS = 20
+NUMBER_OF_DATES = 20
+
 fake = Faker()
 
 sizes = [
     {
-        'name': 'personal',
-        'price': fake.pyfloat(
+        "name": "personal",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=1,
             max_value=5,
-        )
+        ),
     },
     {
-        'name': 'medium',
-        'price': fake.pyfloat(
+        "name": "medium",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=5,
             max_value=10,
-        )
+        ),
     },
     {
-        'name': 'familiar',
-        'price': fake.pyfloat(
+        "name": "familiar",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=10,
             max_value=15,
-        )
+        ),
     },
     {
-        'name': 'large',
-        'price': fake.pyfloat(
+        "name": "large",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=15,
             max_value=20,
-        )
+        ),
     },
     {
-        'name': 'extra large',
-        'price': fake.pyfloat(
+        "name": "extra large",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=20,
             max_value=25,
-        )
+        ),
     },
 ]
 
 ingredients = [
     {
-        'name': 'Jalapeño',
-        'price': fake.pyfloat(
+        "name": "Jalapeño",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=INGREDIENT_MIN_VALUE,
             max_value=INGREDIENT_MAX_VALUE,
-        )
+        ),
     },
     {
-        'name': 'Green Peppers',
-        'price': fake.pyfloat(
+        "name": "Green Peppers",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=INGREDIENT_MIN_VALUE,
             max_value=INGREDIENT_MAX_VALUE,
-        )
+        ),
     },
     {
-        'name': 'Tomato',
-        'price': fake.pyfloat(
+        "name": "Tomato",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=INGREDIENT_MIN_VALUE,
             max_value=INGREDIENT_MAX_VALUE,
-        )
+        ),
     },
     {
-        'name': 'Cheese',
-        'price': fake.pyfloat(
+        "name": "Cheese",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=INGREDIENT_MIN_VALUE,
             max_value=INGREDIENT_MAX_VALUE,
-        )
+        ),
     },
     {
-        'name': 'Mushrooms',
-        'price': fake.pyfloat(
+        "name": "Mushrooms",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=INGREDIENT_MIN_VALUE,
             max_value=INGREDIENT_MAX_VALUE,
-        )
+        ),
     },
     {
-        'name': 'Chicken',
-        'price': fake.pyfloat(
+        "name": "Chicken",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=INGREDIENT_MIN_VALUE,
             max_value=INGREDIENT_MAX_VALUE,
-        )
+        ),
     },
     {
-        'name': 'Oregano',
-        'price': fake.pyfloat(
+        "name": "Oregano",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=INGREDIENT_MIN_VALUE,
             max_value=INGREDIENT_MAX_VALUE,
-        )
+        ),
     },
     {
-        'name': 'Bacon',
-        'price': fake.pyfloat(
+        "name": "Bacon",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=INGREDIENT_MIN_VALUE,
             max_value=INGREDIENT_MAX_VALUE,
-        )
+        ),
     },
     {
-        'name': 'Pepperoni',
-        'price': fake.pyfloat(
+        "name": "Pepperoni",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=INGREDIENT_MIN_VALUE,
             max_value=INGREDIENT_MAX_VALUE,
-        )
+        ),
     },
     {
-        'name': 'Sausage',
-        'price': fake.pyfloat(
+        "name": "Sausage",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=INGREDIENT_MIN_VALUE,
             max_value=INGREDIENT_MAX_VALUE,
-        )
+        ),
     },
 ]
 
 beverages = [
     {
-        'name': 'Coca Cola',
-        'price': fake.pyfloat(
+        "name": "Coca Cola",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=BEVERAGE_MIN_VALUE,
             max_value=BEVERAGE_MAX_VALUE,
-        )
+        ),
     },
     {
-        'name': 'Fanta',
-        'price': fake.pyfloat(
+        "name": "Fanta",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=BEVERAGE_MIN_VALUE,
             max_value=BEVERAGE_MAX_VALUE,
-        )
+        ),
     },
     {
-        'name': 'Sprite',
-        'price': fake.pyfloat(
+        "name": "Sprite",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=BEVERAGE_MIN_VALUE,
             max_value=BEVERAGE_MAX_VALUE,
-        )
+        ),
     },
     {
-        'name': 'Pepsi',
-        'price': fake.pyfloat(
+        "name": "Pepsi",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=BEVERAGE_MIN_VALUE,
             max_value=BEVERAGE_MAX_VALUE,
-        )
+        ),
     },
     {
-        'name': 'Water',
-        'price': fake.pyfloat(
+        "name": "Water",
+        "price": fake.pyfloat(
             left_digits=MAX_LEFT_DIGITS,
             right_digits=MAX_RIGHT_DIGITS,
             positive=True,
             min_value=BEVERAGE_MIN_VALUE,
             max_value=BEVERAGE_MAX_VALUE,
-        )
+        ),
     },
 ]
 
 
 def generate_client():
     return {
-        'client_address': fake.address(),
-        'client_dni': fake.ssn(),
-        'client_name': fake.name(),
-        'client_phone': fake.phone_number(),
+        "client_address": fake.address(),
+        "client_dni": fake.ssn(),
+        "client_name": fake.name(),
+        "client_phone": fake.phone_number(),
     }
+
 
 def seed_size_table():
     for size in sizes:
         SizeManager.create(size)
 
+
 def seed_ingredient_table():
     for ingredient in ingredients:
         IngredientManager.create(ingredient)
+
 
 def seed_beverage_table():
     for beverage in beverages:
         BeverageManager.create(beverage)
 
-def get_sizes_ingredients_beverages():
-    return {
-        'sizes': SizeManager.get_all(),
-        'ingredients': IngredientManager.get_all(),
-        'beverages': BeverageManager.get_all(),
-    }
 
+def get_sizes_ingredients_beverages():
+    return (
+        SizeManager.get_all(),
+        IngredientManager.get_all(),
+        BeverageManager.get_all(),
+    )
+
+
+def pass_by_value(function):
+    def wrapper(*args, **kwargs):
+        copy_args = [arg.copy() for arg in args]
+        copy_kwargs = {key: value.copy() for key, value in kwargs.items()}
+        return function(*copy_args, **copy_kwargs)
+
+    return wrapper
+
+
+@pass_by_value
+def get_random_item(items: list):
+    return random.choice(items)
+
+
+@pass_by_value
+def get_random_items(items: list):
+    return random.choices(items, k=random.randint(1, len(items)))
+
+
+@pass_by_value
+def get_total_price(*args):
+    total = 0
+    for arg in args:
+        if isinstance(arg, list):
+            total += sum([item["price"] for item in arg])
+        else:
+            total += arg["price"]
+    return total
+
+
+def seed_order_table():
+    (sizes, ingredients, beverages) = get_sizes_ingredients_beverages()
+    clients = [generate_client() for _ in range(NUMBER_OF_CLIENTS)]
+    dates = [
+        fake.date_time_between(start_date=MIN_DATE, end_date=MAX_DATE, tzinfo=None)
+        for _ in range(NUMBER_OF_DATES)
+    ]
+
+    for _ in range(MAX_ORDERS):
+        size = get_random_item(sizes)
+        ingredients_detail = get_random_items(ingredients)
+        beverages_detail = get_random_items(beverages)
+        total_price = get_total_price(size, ingredients_detail, beverages_detail)
+        order_data = get_random_item(clients) | {
+            "date": get_random_item(dates),
+            "size_id": size["_id"],
+            "total_price": total_price,
+        }
+        OrderManager.create(order_data, ingredients_detail, beverages_detail)
 
 
 def execute_seeder():
     seed_size_table()
     seed_ingredient_table()
     seed_beverage_table()
-    print(get_sizes_ingredients_beverages())
+    seed_order_table()
