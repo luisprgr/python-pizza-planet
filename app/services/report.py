e39e1e9f2a5c7bdb7668bec1ed18cdc34539bc77
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-03 22:26:44 -0500

    feat: report service

---
+++ app/services/report.py
@@ -0,0 +1,15 @@
+import re
+from app.common.http_methods import GET, POST, PUT
+from flask import Blueprint, jsonify
+
+from ..controllers import ReportController
+
+report = Blueprint('report', __name__)
+
+
+@report.route('/', methods=GET)
+def get_report():
+    report, error = ReportController.get_report()
+    response = report if not error else {'error': error}
+    status_code = 200 if not error else 400
+    return jsonify(response), status_code
