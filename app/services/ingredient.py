ff27dc52030f0411bc8d37974e71cb16aed9c195
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-04 23:57:05 -0500

    fix: update method in ingredient service builder

--- app/services/ingredient.py
+++ app/services/ingredient.py
@@ -1,3 +1,5 @@
+from flask import request, jsonify
+from app.common.http_methods import PUT
 from ..controllers import IngredientController
 from .service_builder import ServiceBuilder
 
@@ -6,6 +8,17 @@ class IngredientServiceBuilder(ServiceBuilder):
     controller = IngredientController
     name = 'ingredient'
     import_name = __name__
+
+    @classmethod
+    def build_update_method(cls):
+
+        def update_ingredient():
+            ingredient, error = IngredientController.update(request.json)
+            response = ingredient if not error else {'error': error}
+            status_code = 200 if not error else 400
+            return jsonify(response), status_code
+        
+        cls.result.add_url_rule('/', view_func=update_ingredient, methods=PUT)
 
 
 ingredient = IngredientServiceBuilder.build()
