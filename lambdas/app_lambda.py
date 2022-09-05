c21b1823d500065c0992f0e225d878fef0b736da
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-05 10:52:20 -0500

    feat: adapting the app to deploy it with sam

---
+++ lambdas/app_lambda.py
@@ -0,0 +1,9 @@
+from flask import Flask
+from flask_lambda import FlaskLambda
+from app import register_blueprints, register_plugins, cors_app
+
+app_lambda = FlaskLambda(__name__)
+app_lambda.config.from_object('app.settings.Config')
+register_blueprints(app_lambda)
+register_plugins(app_lambda)
+cors_app(app_lambda)
