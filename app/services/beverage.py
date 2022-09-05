00a8ac888f8db4109a602c24d3c7acda5a919e6b
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-04 22:49:26 -0500

    refactor: removing unnecesary imports from beverage service

--- app/services/beverage.py
+++ app/services/beverage.py
@@ -1,6 +1,3 @@
-from app.common.http_methods import GET, POST, PUT
-from flask import Blueprint, jsonify, request
-
 from ..controllers import BeverageController
 from .service_builder import ServiceBuilder
 
