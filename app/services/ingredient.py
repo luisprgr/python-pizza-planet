f902636a9060241d8ccbaf75c33dd969d38d980f
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-04 23:59:10 -0500

    fix: linter errors

--- app/services/ingredient.py
+++ app/services/ingredient.py
@@ -11,13 +11,12 @@ class IngredientServiceBuilder(ServiceBuilder):
 
     @classmethod
     def build_update_method(cls):
-
         def update_ingredient():
             ingredient, error = IngredientController.update(request.json)
             response = ingredient if not error else {'error': error}
             status_code = 200 if not error else 400
             return jsonify(response), status_code
-        
+
         cls.result.add_url_rule('/', view_func=update_ingredient, methods=PUT)
 
 
