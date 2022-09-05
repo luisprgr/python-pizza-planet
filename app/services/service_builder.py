91d9506eeb3f3fc17e0000b17f8b1f1db2b3c8b7
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-04 23:51:19 -0500

    refactor: build steps now use clousures to facilitate redefining

--- app/services/service_builder.py
+++ app/services/service_builder.py
@@ -10,45 +10,59 @@ class ServiceBuilder:
 
     @classmethod
     def build_create_method(cls):
-        item, error = cls.controller.create(request.json)
-        response = item if not error else {'error': error}
-        status_code = 200 if not error else 400
-        return jsonify(response), status_code
+        
+        def create_method():
+            item, error = cls.controller.create(request.json)
+            response = item if not error else {'error': error}
+            status_code = 200 if not error else 400
+            return jsonify(response), status_code
+        
+        cls.result.add_url_rule('/', view_func=create_method, methods=POST)
 
     @classmethod
-    def build_update_method(cls, _id: int):
-        item, error = cls.controller.update({"_id": _id} | request.json)
-        response = item if not error else {'error': error}
-        status_code = 200 if not error else 400
-        return jsonify(response), status_code
+    def build_update_method(cls):
+        
+        def update_method(_id: int):
+            item, error = cls.controller.update({"_id": _id} | request.json)
+            response = item if not error else {'error': error}
+            status_code = 200 if not error else 400
+            return jsonify(response), status_code
+
+        cls.result.add_url_rule('/<_id>', view_func=update_method, methods=PUT)
 
     @classmethod
-    def build_get_by_id_method(cls, _id: int):
-        item, error = cls.controller.get_by_id(_id)
-        response = item if not error else {'error': error}
-        status_code = 200 if item else 404 if not error else 400
-        return jsonify(response), status_code
+    def build_get_by_id_method(cls):
+
+        def get_by_id_method(_id: int):
+            item, error = cls.controller.get_by_id(_id)
+            response = item if not error else {'error': error}
+            status_code = 200 if item else 404 if not error else 400
+            return jsonify(response), status_code
+        
+        cls.result.add_url_rule('/id/<_id>', view_func=get_by_id_method, methods=GET)
 
     @classmethod
     def build_get_all_method(cls):
-        items, error = cls.controller.get_all()
-        response = items if not error else {'error': error}
-        status_code = 200 if items else 404 if not error else 400
-        return jsonify(response), status_code
 
+        def get_all_method():
+            items, error = cls.controller.get_all()
+            response = items if not error else {'error': error}
+            status_code = 200 if items else 404 if not error else 400
+            return jsonify(response), status_code
+
+        cls.result.add_url_rule('/', view_func=get_all_method, methods=GET)
+
+        
+    @classmethod
+    def build_blueprint(cls):
+        cls.result = Blueprint(cls.name, cls.import_name)
+        
     @classmethod
     def build(cls):
-        cls.result = Blueprint(cls.name, cls.import_name)
-        cls.result.add_url_rule(
-            '/', methods=POST , view_func=cls.build_create_method
-        )
-        cls.result.add_url_rule(
-            '/<_id>', methods=PUT, view_func=cls.build_update_method
-        )
-        cls.result.add_url_rule(
-            '/id/<_id>', methods=GET, view_func=cls.build_get_by_id_method
-        )
-        cls.result.add_url_rule(
-            '/', methods=GET, view_func=cls.build_get_all_method
-        )
+        cls.build_blueprint()
+        cls.build_create_method()
+        cls.build_update_method()
+        cls.build_get_by_id_method()
+        cls.build_get_all_method()
+
         return cls.result
