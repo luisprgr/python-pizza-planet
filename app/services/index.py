70490a54289ee8b45fd9a8a57594c396aa363bed
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-05 00:18:15 -0500

    refactor: implementing service builder in index service

--- app/services/index.py
+++ app/services/index.py
@@ -1,18 +1,35 @@
 from app.common.http_methods import GET
-from flask import Blueprint, jsonify
+from flask import jsonify
 
 from ..controllers import IndexController
 
-index = Blueprint('index', __name__)
+from .service_builder import ServiceBuilder
 
 
-@index.route('/', methods=GET)
-def get_index():
-    is_database_up, error = IndexController.test_connection()
-    return jsonify(
-        {
-            'version': '0.0.2',
-            'status': 'up' if is_database_up else 'down',
-            'error': error,
-        }
-    )
+class IndexServiceBuilder(ServiceBuilder):
+    controller = IndexController
+    name = 'index'
+    import_name = __name__
+
+    @classmethod
+    def build_get_index_method(cls):
+        def get_index():
+            is_database_up, error = IndexController.test_connection()
+            return jsonify(
+                {
+                    'version': '0.0.2',
+                    'status': 'up' if is_database_up else 'down',
+                    'error': error,
+                }
+            )
+
+        cls.result.add_url_rule('/', view_func=get_index, methods=GET)
+
+    @classmethod
+    def build(cls):
+        cls.build_blueprint()
+        cls.build_get_index_method()
+        return cls.result
+
+
+index = IndexServiceBuilder.build()
