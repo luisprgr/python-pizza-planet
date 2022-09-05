52ad5d694d279959d58d3ab518b23551dc20e93a
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-04 22:33:44 -0500

    style: pep8 in service_builder

--- app/services/service_builder.py
+++ app/services/service_builder.py
@@ -15,21 +15,21 @@ class ServiceBuilder:
         response = item if not error else {'error': error}
         status_code = 200 if not error else 400
         return jsonify(response), status_code
-    
+
     @classmethod
     def build_update_method(cls, _id: int):
         item, error = cls.controller.update({"_id": _id} | request.json)
         response = item if not error else {'error': error}
         status_code = 200 if not error else 400
         return jsonify(response), status_code
-    
+
     @classmethod
     def build_get_by_id_method(cls, _id: int):
         item, error = cls.controller.get_by_id(_id)
         response = item if not error else {'error': error}
         status_code = 200 if item else 404 if not error else 400
         return jsonify(response), status_code
-    
+
     @classmethod
     def build_get_all_method(cls):
         items, error = cls.controller.get_all()
@@ -40,8 +40,16 @@ class ServiceBuilder:
     @classmethod
     def build(cls):
         cls.result = Blueprint(cls.name, cls.import_name)
-        cls.result.add_url_rule('/', methods=['POST'], view_func=cls.build_create_method)
-        cls.result.add_url_rule('/<_id>', methods=['PUT'], view_func=cls.build_update_method)
-        cls.result.add_url_rule('/id/<_id>', methods=['GET'], view_func=cls.build_get_by_id_method)
-        cls.result.add_url_rule('/', methods=['GET'], view_func=cls.build_get_all_method)
+        cls.result.add_url_rule(
+            '/', methods=['POST'], view_func=cls.build_create_method
+        )
+        cls.result.add_url_rule(
+            '/<_id>', methods=['PUT'], view_func=cls.build_update_method
+        )
+        cls.result.add_url_rule(
+            '/id/<_id>', methods=['GET'], view_func=cls.build_get_by_id_method
+        )
+        cls.result.add_url_rule(
+            '/', methods=['GET'], view_func=cls.build_get_all_method
+        )
         return cls.result
