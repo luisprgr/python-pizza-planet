ba98a0d4aeda126d253afe5c2af0c92671323159
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-09-05 08:25:08 -0500

    [PIZ3] Design Patterns (#24)
    * feat: adding service builder (Builder design pattern)
    
    * style: pep8 in service_builder
    
    * refactor: implementing service builder in beverage service
    
    * fix: beverage service import error
    
    * refactor: using app http_methods for the service builder
    
    * refactor: removing unnecesary imports from beverage service
    
    * refactor: implementing service builder in ingredient service
    
    * refactor: build steps now use clousures to facilitate redefining
    
    * fix: update method in ingredient service builder
    
    * fix: linter errors
    
    * refactor: implementing service builder in order service
    
    * refactor: implementing service builder in report service
    
    * refactor: implementing service builder in size service
    
    * refactor: implementing service builder in index service

--- app/services/ingredient.py
+++ app/services/ingredient.py
@@ -1,38 +1,23 @@
-from app.common.http_methods import GET, POST, PUT
-from flask import Blueprint, jsonify, request
-
+from flask import request, jsonify
+from app.common.http_methods import PUT
 from ..controllers import IngredientController
-
-ingredient = Blueprint('ingredient', __name__)
-
-
-@ingredient.route('/', methods=POST)
-def create_ingredient():
-    ingredient, error = IngredientController.create(request.json)
-    response = ingredient if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+from .service_builder import ServiceBuilder
 
 
-@ingredient.route('/', methods=PUT)
-def update_ingredient():
-    ingredient, error = IngredientController.update(request.json)
-    response = ingredient if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+class IngredientServiceBuilder(ServiceBuilder):
+    controller = IngredientController
+    name = 'ingredient'
+    import_name = __name__
+
+    @classmethod
+    def build_update_method(cls):
+        def update_ingredient():
+            ingredient, error = IngredientController.update(request.json)
+            response = ingredient if not error else {'error': error}
+            status_code = 200 if not error else 400
+            return jsonify(response), status_code
+
+        cls.result.add_url_rule('/', view_func=update_ingredient, methods=PUT)
 
 
-@ingredient.route('/id/<_id>', methods=GET)
-def get_ingredient_by_id(_id: int):
-    ingredient, error = IngredientController.get_by_id(_id)
-    response = ingredient if not error else {'error': error}
-    status_code = 200 if ingredient else 404 if not error else 400
-    return jsonify(response), status_code
-
-
-@ingredient.route('/', methods=GET)
-def get_ingredients():
-    ingredients, error = IngredientController.get_all()
-    response = ingredients if not error else {'error': error}
-    status_code = 200 if ingredients else 404 if not error else 400
-    return jsonify(response), status_code
+ingredient = IngredientServiceBuilder.build()
--- app/services/report.py
+++ app/services/report.py
@@ -1,14 +1,30 @@
 from app.common.http_methods import GET
-from flask import Blueprint, jsonify
+from flask import jsonify
 
 from ..controllers import ReportController
+from .service_builder import ServiceBuilder
 
-report = Blueprint('report', __name__)
+
+class ReportServiceBuilder(ServiceBuilder):
+    controller = ReportController
+    name = 'report'
+    import_name = __name__
+
+    @classmethod
+    def build_get_report_method(cls):
+        def get_report():
+            report, error = ReportController.get_report()
+            response = report if not error else {'error': error}
+            status_code = 200 if not error else 400
+            return jsonify(response), status_code
+
+        cls.result.add_url_rule('/', view_func=get_report, methods=GET)
+
+    @classmethod
+    def build(cls):
+        cls.build_blueprint()
+        cls.build_get_report_method()
+        return cls.result
 
 
-@report.route('/', methods=GET)
-def get_report():
-    report, error = ReportController.get_report()
-    response = report if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+report = ReportServiceBuilder.build()
