f1229eb3c9f2939e309627ca667efaaf512ceef7
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-10-22 00:09:09 -0500

    refactor(size service): restoring size service to commit e861d9807d05445ccada3b4c2f780e8a15d21cd6

--- app/services/size.py
+++ app/services/size.py
@@ -1,12 +1,38 @@
+from app.common.http_methods import GET, POST, PUT
+from flask import Blueprint, jsonify, request
+
 from ..controllers import SizeController
 
-from .service_builder import ServiceBuilder
+size = Blueprint('size', __name__)
 
 
-class SizeServiceBuilder(ServiceBuilder):
-    controller = SizeController
-    name = 'size'
-    import_name = __name__
+@size.route('/', methods=POST)
+def create_size():
+    size, error = SizeController.create(request.json)
+    response = size if not error else {'error': error}
+    status_code = 200 if not error else 400
+    return jsonify(response), status_code
 
 
-size = SizeServiceBuilder.build()
+@size.route('/<_id>', methods=PUT)
+def update_size(_id: int):
+    size, error = SizeController.update({"_id": _id} | request.json)
+    response = size if not error else {'error': error}
+    status_code = 200 if not error else 400
+    return jsonify(response), status_code
+
+
+@size.route('/id/<_id>', methods=GET)
+def get_size_by_id(_id: int):
+    size, error = SizeController.get_by_id(_id)
+    response = size if not error else {'error': error}
+    status_code = 200 if size else 404 if not error else 400
+    return jsonify(response), status_code
+
+
+@size.route('/', methods=GET)
+def get_sizes():
+    sizes, error = SizeController.get_all()
+    response = sizes if not error else {'error': error}
+    status_code = 200 if sizes else 404 if not error else 400
+    return jsonify(response), status_code
