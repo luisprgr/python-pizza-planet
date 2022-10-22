6193b05bf883c54cdcb7f54493d39be5ceaaae0d
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-10-22 00:31:47 -0500

    fix(service decorator): using functools wraps to avoid function overwriting

--- app/services/service_decorator.py
+++ app/services/service_decorator.py
@@ -1,10 +1,13 @@
 from flask import jsonify
+from functools import wraps
 
 
-def service_decorator(service):
+def service(service_function):
+    @wraps(service_function)
     def wrapper(*args, **kwargs):
-        items, error = service(*args, **kwargs)
-        response = items if not error else {'error': error}
+        items, error = service_function(*args, **kwargs)
+        response = items if not error else {"error": error}
         status_code = 200 if items else 404 if not error else 400
         return jsonify(response), status_code
+
     return wrapper
