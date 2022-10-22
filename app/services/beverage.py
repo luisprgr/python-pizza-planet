8f134a8c219f77e256ab0e0eeb9ef51f9da79de0
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-10-22 00:33:58 -0500

    refactor(beverage service): using service decorator in beverage service

--- app/services/beverage.py
+++ app/services/beverage.py
@@ -1,38 +1,31 @@
 from app.common.http_methods import GET, POST, PUT
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, request
 
 from ..controllers import BeverageController
+from app.services.service_decorator import service
 
-beverage = Blueprint('beverage', __name__)
+beverage = Blueprint("beverage", __name__)
 
 
-@beverage.route('/', methods=POST)
+@beverage.route("/", methods=POST)
+@service
 def create_beverage():
-    beverage, error = BeverageController.create(request.json)
-    response = beverage if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+    return BeverageController.create(request.json)
 
 
-@beverage.route('/<_id>', methods=PUT)
+@beverage.route("/<_id>", methods=PUT)
+@service
 def update_beverage(_id: int):
-    beverage, error = BeverageController.update({"_id": _id} | request.json)
-    response = beverage if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+    return BeverageController.update({"_id": _id} | request.json)
 
 
-@beverage.route('/id/<_id>', methods=GET)
+@beverage.route("/id/<_id>", methods=GET)
+@service
 def get_beverage_by_id(_id: int):
-    beverage, error = BeverageController.get_by_id(_id)
-    response = beverage if not error else {'error': error}
-    status_code = 200 if beverage else 404 if not error else 400
-    return jsonify(response), status_code
+    return BeverageController.get_by_id(_id)
 
 
-@beverage.route('/', methods=GET)
+@beverage.route("/", methods=GET)
+@service
 def get_beverages():
-    beverages, error = BeverageController.get_all()
-    response = beverages if not error else {'error': error}
-    status_code = 200 if beverages else 404 if not error else 400
-    return jsonify(response), status_code
+    return BeverageController.get_all()
