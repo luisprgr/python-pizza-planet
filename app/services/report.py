ce4a7fc59102f39ebe1791988c9d5cf3f9e363f9
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-10-22 00:46:49 -0500

    refactor(report service): using service decorator in report service

--- app/services/report.py
+++ app/services/report.py
@@ -1,14 +1,13 @@
 from app.common.http_methods import GET
-from flask import Blueprint, jsonify
+from flask import Blueprint
 
 from ..controllers import ReportController
+from app.services.service_decorator import service
 
 report = Blueprint('report', __name__)
 
 
 @report.route('/', methods=GET)
+@service
 def get_report():
-    report, error = ReportController.get_report()
-    response = report if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+    return ReportController.get_report()
