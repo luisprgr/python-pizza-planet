8b03f4c4f3170d3b27641af4743d05284388f65d
luisprgr <50538689+luisprgr@users.noreply.github.com>
2022-10-21 23:55:27 -0500

    refactor(index service): restoring index service to commit becd1916c7e0e3310864b7c0f1b801092a9bd67f

--- app/services/index.py
+++ app/services/index.py
@@ -1,35 +1,18 @@
 from app.common.http_methods import GET
-from flask import jsonify
+from flask import Blueprint, jsonify
 
 from ..controllers import IndexController
 
-from .service_builder import ServiceBuilder
+index = Blueprint('index', __name__)
 
 
-class IndexServiceBuilder(ServiceBuilder):
-    controller = IndexController
-    name = 'index'
-    import_name = __name__
-
-    @classmethod
-    def build_get_index_method(cls):
-        def get_index():
-            is_database_up, error = IndexController.test_connection()
-            return jsonify(
-                {
-                    'version': '0.0.2',
-                    'status': 'up' if is_database_up else 'down',
-                    'error': error,
-                }
-            )
-
-        cls.result.add_url_rule('/', view_func=get_index, methods=GET)
-
-    @classmethod
-    def build(cls):
-        cls.build_blueprint()
-        cls.build_get_index_method()
-        return cls.result
-
-
-index = IndexServiceBuilder.build()
+@index.route('/', methods=GET)
+def get_index():
+    is_database_up, error = IndexController.test_connection()
+    return jsonify(
+        {
+            'version': '0.0.2',
+            'status': 'up' if is_database_up else 'down',
+            'error': error,
+        }
+    )
