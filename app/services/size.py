85e6f989059691d2dfbaa3d858d62324c25971d7
Luis Paredes <50538689+luisprgr@users.noreply.github.com>
2022-10-22 01:48:03 -0500

    [PIZ18] - Using AWS CloudWatch monitoring tool (#33)
    * feat(logging and monitoring): adding logs to see in cloudwatch
    
    * fix(logs): sanitizing id value

--- app/services/size.py
+++ app/services/size.py
@@ -1,31 +1,36 @@
 from app.common.http_methods import GET, POST, PUT
 from flask import Blueprint, request
+import logging
 
 from ..controllers import SizeController
 from app.services.service_decorator import service
 
-size = Blueprint('size', __name__)
+size = Blueprint("size", __name__)
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 
-@size.route('/', methods=POST)
+@size.route("/", methods=POST)
 @service
 def create_size():
     return SizeController.create(request.json)
 
 
-@size.route('/<_id>', methods=PUT)
+@size.route("/<_id>", methods=PUT)
 @service
 def update_size(_id: int):
     return SizeController.update({"_id": _id} | request.json)
 
 
-@size.route('/id/<_id>', methods=GET)
+@size.route("/id/<_id>", methods=GET)
 @service
 def get_size_by_id(_id: int):
+    logger.info("get size by id endpoint called")
+    logger.info(f"size id: {str(_id)}")
     return SizeController.get_by_id(_id)
 
 
-@size.route('/', methods=GET)
+@size.route("/", methods=GET)
 @service
 def get_sizes():
     return SizeController.get_all()
